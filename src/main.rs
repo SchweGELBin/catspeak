fe042bdf6b229b7fd87174662f28a932eb527949
SchweGELBin <abramjannikmichael06@gmail.com>
2024-06-01 21:31:35 +0200

    feat: add basic functionality

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
+use std::env;
+
 fn main() {
-    println!("Hello, world!");
+    let input = get_text();
+    let out = catspeak(&input);
+    println!("{}", out);
+}
+
+fn catspeak(text: &str) -> String {
+    let cat = r#"
+ |\ /|
+/ . . \
+\ _-_ /      #Text#
+ |   |     _
+ |    \   /
+ |     |-/
+ |_|_ /
+        "#;
+    let cat = cat.replace("#Text#", text);
+    cat.to_string()
+}
+
+fn get_text() -> String {
+    let args: Vec<String> = env::args().collect();
+    if args.len() <= 1 {
+        return "Error: Read the documentation with \"catspeak --help\"".to_string();
+    }
+    let text = &args[1];
+    if text == "--help" {
+        return "Usage: catspeak \"<message>\"".to_string();
+    }
+    text.to_string()
 }
