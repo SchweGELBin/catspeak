213c42bae8643a176fb8182e8f1efc29cc25ef1f
SchweGELBin <abramjannikmichael06@gmail.com>
2024-06-03 18:35:06 +0200

    feat: add option parameter

--- src/main.rs
+++ src/main.rs
@@ -2,24 +2,41 @@ use std::env;
 
 fn main() {
     let input = get_text();
-    let out = catspeak(&input);
+    let out = catspeak(&input.0, input.1);
     println!("{}", out);
 }
 
-fn catspeak(text: &str) -> String {
-    let cat = include_str!("../res/cat.txt");
-    let cat = cat.replace("#Text#", text);
-    cat.to_string()
+fn catspeak(text: &str, cat_index: Option<u8>) -> String {
+    let mut cat = text.to_string();
+    if cat_index.is_some() {
+        cat = include_str!("../res/cat.txt").replace("#Text#", text);
+    }
+    cat
 }
 
-fn get_text() -> String {
+fn get_text() -> (String, Option<u8>) {
+    let err_msg = "Error: See \"catspeak --help\"".to_string();
     let args: Vec<String> = env::args().collect();
     if args.len() <= 1 {
-        return "Error: Read the documentation with \"catspeak --help\"".to_string();
+        return (err_msg, None);
     }
-    let text = &args[1];
-    if text == "--help" {
-        return "Usage: catspeak \"<message>\"".to_string();
+    let mut option = String::new();
+    let mut text = &args[1];
+    if args[1].starts_with("-") {
+        if args.len() <= 2 {
+            if &args[1] != "--help" && &args[1] != "-h"{
+                return (err_msg, None);
+            }
+        } else {
+            text = &args[2];
+        }
+        option = args[1].clone();
     }
-    text.to_string()
+    if &args[1] == "--help" || &args[1] == "-h" {
+        return ("Usage: catspeak <option> \"<message>\"".to_string(), None);
+    }
+    if option == "--random" || option == "-r" {
+        return (text.to_string(), Some(0));
+    }
+    (text.to_string(), Some(0))
 }
